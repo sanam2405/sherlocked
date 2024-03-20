c431539e65eb603274abaf39ee3dd88277aa4f25
Manas Pratim Biwas <manaspb2405@gmail.com>
2024-03-21 03:52:47 +0530

    [feat]: add ls,pwd,cd,cat commands

--- ncserver/src/index.ts
+++ ncserver/src/index.ts
@@ -8,39 +8,64 @@ let currentDirectory = process.cwd();
 // Create a server instance
 const server = createServer((socket: Socket) => {
   console.log('Client connected');
-
+  socket.write('You are connected to Sherlocked 2024! \n\nsanam@sherlocked: ')
   // Event listener for data received from clients
   socket.on('data', (data: Buffer) => {
     const command = data.toString().trim();
     console.log(`Received command: ${command}`);
 
-    // Attempt to get present working directory
+    // Get all the files present in the current working directory
     if (command === 'ls') {
     exec('ls', { cwd: currentDirectory }, (error, stdout, stderr) => {
     if (error) {
-      socket.write(`Error: ${error.message}\n-------------------------\nsherlocked@2024:`);
+      socket.write('\n-------------------------\n')
+      socket.write(`sanam@sherlocked: Error: ${error.message}\n-------------------------\nsanam@sherlocked: `);
     } else {
       if (stderr) {
-        socket.write(`stderr: ${stderr}\n-------------------------\nsherlocked@2024:`);
+        socket.write('\n-------------------------\n')
+        socket.write(`sanam@sherlocked: stderr: ${stderr}\n-------------------------\nsanam@sherlocked: `);
       } else {
-        socket.write(`Current files in the directory: \n${stdout}`);
+        socket.write('\n-------------------------\n')
+        socket.write(`sanam@sherlocked: Current files in the directory: \n\n${stdout}\n-------------------------\nsanam@sherlocked: `);
+      }
+    }
+  });
+}
+
+// Get the content of a particular file
+else if (command.startsWith('cat')) {
+    const args = command.split(' ');
+    const file = args[1] || ''; // Extract directory from the command
+
+    exec(`cat ${file}`, { cwd: currentDirectory }, (error, stdout, stderr) => {
+    if (error) {
+      socket.write('\n-------------------------\n')
+      socket.write(`sanam@sherlocked: Error: ${error.message}\n-------------------------\nsanam@sherlocked: `);
+    } else {
+      if (stderr) {
+        socket.write('\n-------------------------\n')
+        socket.write(`sanam@sherlocked: stderr: ${stderr}\n-------------------------\nsanam@sherlocked: `);
+      } else {
+        socket.write(`Content of ${file}: \n\n${stdout}\n-------------------------\nsanam@sherlocked: `);
       }
     }
   });
 }
     
-    // Attempt to get present working directory
+    // Get the present working directory
     else if (command === 'pwd') {
       exec(`pwd`, (error) => {
         if (error) {
-          socket.write(`Error: ${error.message}\n-------------------------\nsherlocked@2024:`);
+          socket.write('\n-------------------------\n')
+          socket.write(`sanam@sherlocked: Error: ${error.message}\n-------------------------\nsanam@sherlocked: `);
         } else {
-          socket.write(`Current directory to: ${currentDirectory}\n-------------------------\nsherlocked@2024:`);
+          socket.write('\n-------------------------\n')
+          socket.write(`sanam@sherlocked: Current directory to: ${currentDirectory}\n-------------------------\nsanam@sherlocked: `);
         }
       });
     }
 
-    // Handle 'cd' command separately
+    // Handle change directory command separately
     else if (command.startsWith('cd')) {
       const args = command.split(' ');
       const directory = args[1] || ''; // Extract directory from the command
@@ -49,25 +74,30 @@ const server = createServer((socket: Socket) => {
       // Attempt to change directory
       exec(`cd ${newPath}`, (error) => {
         if (error) {
-          socket.write(`Error: ${error.message}\n-------------------------\nsherlocked@2024:`);
+          socket.write('\n-------------------------\n')
+          socket.write(`sanam@sherlocked: Error: ${error.message}\n-------------------------\nsanam@sherlocked: `);
         } else {
           currentDirectory = newPath; // Update the current directory
-          socket.write(`Changed directory to: ${currentDirectory}\n`);
+          socket.write('\n-------------------------\n')
+          socket.write(`sanam@sherlocked: Changed directory to: ${currentDirectory}\nsanam@sherlocked: `);
         }
       });
     } else {
       // Execute other commands using child_process.exec()
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          socket.write(`Error: ${error.message}\n-------------------------\nsherlocked@2024:`);
-          return;
-        }
-        if (stderr) {
-          socket.write(`stderr: ${stderr}\n`);
-          return;
-        }
-        socket.write(`${stdout}\n--------------------------\nsherlocked@2024:sherlocked@2024:`);
-      });
+      // exec(command, (error, stdout, stderr) => {
+      //   if (error) {
+      //     socket.write('\n-------------------------\n')
+      //     socket.write(`sanam@sherlocked: Error: ${error.message}\n-------------------------\nsanam@sherlocked: `);
+      //     return;
+      //   }
+      //   if (stderr) {
+      //     socket.write('\n-------------------------\n')
+      //     socket.write(`sanam@sherlocked: stderr: ${stderr}sanam@sherlocked: `);
+      //     return;
+      //   }
+      //   socket.write(`${stdout}\n--------------------------sanam@sherlocked: `);
+      // });
+      socket.write(`sanam@sherlocked: That's what she said\nsanam@sherlocked: `);
     }
   });
 
