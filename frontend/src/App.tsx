b33288f463f2898f80bd44e917ecf297b70dbc6b
Manas Pratim Biwas <manaspb2405@gmail.com>
2024-03-23 00:31:44 +0530

    [feat]: error page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import Level0 from "./pages/level0.jsx";
-import Level1 from "./pages/level1.jsx";
-import Level2 from "./pages/level2.jsx";
-import Level3 from "./pages/level3.jsx";
+import Login from "./pages/Login";
+import Level1 from "./pages/level1";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
@@ -11,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+           <Route path="/error-page" element={<ErrorPage />} />
           <Route path="/level-1" element={<Level1 />} />
           {/* <Route path='/level-2' element={<Level2 />} />
 							<Route path='/level-3' element={<Level3 />} />
---
+++ frontend/src/pages/ErrorPage.tsx
@@ -0,0 +1,9 @@
+import "../styles/errorpage.css"
+
+const ErrorPage = () => {
+    return <div className="error-container">
+        <img src="/errormeme.gif" alt="Error" />
+    </div>
+}
+
+export default ErrorPage
