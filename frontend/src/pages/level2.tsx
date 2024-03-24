e0fbfad58b6f8d0e67201bf4631128b96a73febf
Manas Pratim Biwas <manaspb2405@gmail.com>
2024-03-24 15:30:56 +0530

    [fix]: level2 hints

--- frontend/src/pages/level2.tsx
+++ frontend/src/pages/level2.tsx
@@ -124,7 +124,8 @@ const Level2 = () => {
 						Oops the message sank into this page
 						<span style={{ display: 'none' }}>
 							"What a fine day it was when we met for the first time" Send her a
-							"msg" : "I love you Mia" to our backend route /flirt
+							POST request to our backend route /flirt with the payload "msg" :
+							"I love you Mia"
 						</span>
 					</p>
 					<form onSubmit={event => postData(event)} className='form'>
